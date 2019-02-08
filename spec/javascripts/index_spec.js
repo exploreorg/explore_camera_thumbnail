159c2c64995a4273e8b6bf04c0cb625f4f7b08f9
Doc Ritezel and Matt Olenick <pair+doc+matto@ministryofvelocity.com>
2019-02-07 18:17:50 -0800

    Partner can see a demo page discussing Thumbnail embedding

---
+++ spec/javascripts/index_spec.js
@@ -0,0 +1,67 @@
+describe('CameraVideoThumbnail', () => {
+  let root, fakeHls
+
+  beforeEach((done) => {
+    root = document.querySelector('#jasmine_content')
+    fakeHls = function () {}
+    fakeHls.prototype.on = jasmine.createSpy('on')
+    fakeHls.prototype.loadSource = jasmine.createSpy('loadSource')
+    fakeHls.prototype.destroy = jasmine.createSpy('destroy')
+    fakeHls.Events = Hls.Events
+    ReactDOM.render(
+      <CameraVideoThumbnail url='http://example.com/video.m3u8' host='example.org' hlsDriver={fakeHls} name='Good Times' src='http://example.com/bars.png' />,
+      root
+    )
+    setImmediate(done)
+  })
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(root)
+  })
+
+  it('renders a container', () => {
+    expect(root.querySelector('.camera-video-thumbnail')).not.toBeNull()
+  })
+
+  it('renders an image', () => {
+    expect(root.querySelector('.camera-video-thumbnail__image')).not.toBeNull()
+  })
+
+  it('renders an image with a blank source', () => {
+    expect(root.querySelector('.camera-video-thumbnail__image').getAttribute('src')).toEqual('http://example.com/bars.png')
+  })
+
+  it('renders an image with an alternative description', () => {
+    expect(root.querySelector('.camera-video-thumbnail__image').getAttribute('alt')).toEqual('Good Times')
+  })
+
+  it('binds the level load event', () => {
+    expect(fakeHls.prototype.on).toHaveBeenCalledWith(Hls.Events.LEVEL_LOADED, jasmine.any(Function))
+  })
+
+  it('loads the url', () => {
+    expect(fakeHls.prototype.loadSource).toHaveBeenCalledWith('http://example.com/video.m3u8')
+  })
+
+  describe('when the component has been unmounted', () => {
+    beforeEach(() => {
+      ReactDOM.unmountComponentAtNode(root)
+    })
+
+    it('destroys the hls instance', () => {
+      expect(fakeHls.prototype.destroy).toHaveBeenCalled()
+    })
+  })
+
+  describe('when the level load handler is called', () => {
+    beforeEach((done) => {
+      const fakeFragment = { url: 'http://example.com/segments/1.ts' }
+      fakeHls.prototype.on.calls.mostRecent().args[1]({}, { details: { fragments: [fakeFragment, {}, {}, {}] } })
+      setImmediate(done)
+    })
+
+    it('sets the source of the image', () => {
+      expect(root.querySelector('.camera-video-thumbnail__image').getAttribute('src')).toEqual('http://example.org/segments/1.ts.jpg')
+    })
+  })
+})
---
+++ spec/javascripts/support/jasmine_helper.rb
@@ -0,0 +1,22 @@
+# Use this file to set/override Jasmine configuration options
+# You can remove it if you don't need it.
+# This file is loaded *after* jasmine.yml is interpreted.
+#
+# Example: using a different boot file.
+# Jasmine.configure do |config|
+#    config.boot_dir = '/absolute/path/to/boot_dir'
+#    config.boot_files = lambda { ['/absolute/path/to/boot_dir/file.js'] }
+# end
+#
+# Example: prevent PhantomJS auto install, uses PhantomJS already on your path.
+Jasmine.configure do |config|
+  if ENV['TRAVIS']
+    config.prevent_phantom_js_auto_install = true
+  end
+end
+
+module Jasmine
+  def self.runner_template
+    File.read(File.join(File.dirname(__FILE__), "run.html.erb"))
+  end
+end
