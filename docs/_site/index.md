2a052d3571d2fa94eb0fda6f3dcbd634eb2c3c25
Matt Olenick <matt.olenick@gmail.com>
2019-02-08 10:32:28 -0800

    Restore main-bundle.js and remove _site directory

--- docs/_site/index.md
+++
@@ -1,7 +0,0 @@
-# Explore Camera Thumbnail Demo
-
-This demonstrates a thumbnail viewer for an Explore.org camera stream.
-
-It requires an access token, stream ID and a URL.
-
-<div data-react-component="CameraVideoThumbnail" data-react-props='{"url":"https://example.com","host":"example.com","name":"Camera","src":"https://placekitten.com/g/1600/900"}'></div>
--- docs/_site/javascripts/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import * as Components from '../../lib/index'
-
-Array.from(document.querySelectorAll('[data-react-component][data-react-props]')).forEach((element) => {
-  const props = JSON.parse(element.dataset.reactProps)
-  const Component = Components[element.dataset.reactComponent]
-  ReactDOM.render(<Component {...props} />, element)
-})
